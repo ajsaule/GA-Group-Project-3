2a4ed9ec15e80fb34d6fc5cccbbbc568b13e2189
PhillipeAlves <phillipe.alves@gmail.com>
2020-08-14 13:32:08 +1000

    background banner

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -13,11 +13,9 @@ function logIn(event) {
 function loginForm() {
     let template = `
         <div class="login-signup">
-            <h3>Log in</h3>
+            <h3>LOG IN</h3>
             <form onSubmit="session(event)" class="form-styling" action="" method="post">
-                <label for="">email</label>
                 <input class="forms" name="email" type="text" placeholder="Email" autocomplete="off">
-                <label for="">password</label>
                 <input class="forms" name="password" type="password" placeholder="Password" autocomplete="off">
                 <button class="login-btn">Login</button>
             </form>
@@ -35,6 +33,7 @@ function session(e) {
     axios.post('/session', formDataObj).then(res => {
         if (res.data.message === "logged in") {
             createlogin.appendChild(confirmLogin(res.data.id))
+            renderLogin()
         }
     })
 }
@@ -45,13 +44,28 @@ function confirmLogin(userId) {
         <div class="login-signup">
             <h3>You have successfully logged in.</h3>
             <a class="forms" onClick="home(event)" href="#">Home</a>
-            <a class="forms delete-account" onClick="deleteAccount(event)" href="#" data-id="${userId}"><i class="fas fa-trash-alt"></i> DELETE ACCOUNT</a>
+            <a class="forms delete-account" onClick="deleteAccount(event)" href="#" data-id="${userId}"><i class="fas fa-user-minus"></i> DELETE ACCOUNT</a>
         </div>
     `
     let newArticle = document.createElement('article')
     newArticle.innerHTML = template
-    loggedIn.innerHTML = ''
-    loggingout.innerHTML = 'Log out'
-    signedUp.innerHTML = ''
     return newArticle
 }
+
+function renderLogin() {
+    axios
+    .get('/session')
+    .then(res => {
+        if (res.data.message === "logged in") {
+            loggedIn.innerHTML = ''
+            loggingout.innerHTML = 'Log out'
+            signedUp.innerHTML = ''
+        } else {
+            loggedIn.innerHTML = 'Log in'
+            loggingout.innerHTML = ''
+            signedUp.innerHTML = 'Sign up'
+        }
+    })
+}
+
+renderLogin()
