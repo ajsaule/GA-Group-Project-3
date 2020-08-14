7438164b01dc761dfce88038c98bdb6a37d48a52
Nandy Rodrigo <nandy.rodrigo@gmail.com>
2020-08-14 13:14:59 +1000

    countdown completed

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -4,4 +4,41 @@ function home(event) {
     let target = event.target
     target.classList.add('active')
     // code here
+    displayCounter()
 }
+
+
+function displayCounter(){
+
+    let counter = document.createElement('p')
+        counter.classList.add('counter')
+
+    let counterStatement = document.createElement('p')
+        counterStatement.classList.add('counterStatement')
+
+    var countDownDate = new Date("Sep 13, 2020 00:00:00").getTime();
+    var x = setInterval(function() {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+        
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      counterStatement.innerHTML = "Days until VICTORIA comes out of STAGE 4 Lockdown: "
+        
+      counter.innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+        
+      if (distance < 0) {
+        clearInterval(x);
+        counter.innerHTML = "STAGE 4 Lockdown is OVER!!!";
+      }
+    }, 1000);
+
+    content.appendChild(counterStatement)
+    content.appendChild(counter)
+
+}
+
